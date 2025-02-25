b041c78ad38b6b5da961464876c40a67b4c1808b
UDAY-DEVELOPER-07 <udaysinghno2005@gmail.com>
2025-02-25 22:10:54 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <>
     <main className="w=full h-16 bg-whiteS flex items-center ">
       <div className='m-3'>
-        <h2 className=' font-Fahkwang text-black text-2xl font-medium'>Grey Image Comparer</h2>
+        <h2 className=' font-Fahkwang text-black text-2xl font-bold'>Grey Image Comparer</h2>
       </div>
     </main>
     </>
